b0edf21cd3499893f23d31c0c4837c400206b9d9
Dave <55182282+dvsu@users.noreply.github.com>
2024-09-07 16:58:36 +0100

    Update import

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from .networktools import NetworkInfo as NetworkInfo
 from .networktools import NetworkTools as NetworkTools
 
-__all__ = ["NetworkTools"]
+__all__ = ["NetworkInfo", "NetworkTools"]
--- networktools/__init__.py
+++ networktools/__init__.py
@@ -1,3 +1,4 @@
+from .models import NetworkInfo as NetworkInfo
 from .networktools import NetworkTools as NetworkTools
 
-__all__ = ["NetworkTools"]
+__all__ = ["NetworkInfo", "NetworkTools"]
